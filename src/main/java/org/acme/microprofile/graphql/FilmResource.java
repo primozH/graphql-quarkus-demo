dd9926aab0307aec070fcae51d161320cd454b18
Primoz Hrovat <primoz.hrovat@bearingpoint.com>
2021-01-25 14:52:42 +0100

    graphql with quarkus

---
+++ src/main/java/org/acme/microprofile/graphql/FilmResource.java
@@ -0,0 +1,46 @@
+package org.acme.microprofile.graphql;
+
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.graphql.*;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@GraphQLApi
+public class FilmResource {
+
+    @Inject
+    GalaxyService service;
+
+    @Query("allFilms")
+    @Description("Get all Films from a galaxy far far away")
+    public List<Film> getAllFilms() {
+        return service.getAllFilms();
+    }
+
+    @Query
+    @Description("Get a Film from a galaxy far far away")
+    public Uni<Film> getFilm(@Name("filmId") int id) {
+        return service.getFilm(id);
+    }
+
+    public List<Hero> heroes(@Source Film film) {
+        return service.getHeroesByFilm(film);
+    }
+
+    @Mutation
+    public Hero createHero(Hero hero) {
+        service.addHero(hero);
+        return hero;
+    }
+
+    @Mutation
+    public Hero deleteHero(int id) {
+        return service.deleteHero(id);
+    }
+
+    @Query
+    public List<Hero> getHeroesWithSurname(@DefaultValue("Skywalker") String surname) {
+        return service.getHeroesBySurname(surname);
+    }
+}
